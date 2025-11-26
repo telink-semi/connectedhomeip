c038765b7918c9674217ce60c409891b7fba8fc9
Haiwen Xia <haiwen.xia@telink-semi.com>
2025-11-26 13:26:01 +0800

    telink: tl3238x: add new soc.
    - add tl3238x into matter.
    
    Signed-off-by: Haiwen Xia <haiwen.xia@telink-semi.com>

--- scripts/build/builders/telink.py
+++ scripts/build/builders/telink.py
@@ -121,6 +121,9 @@ class TelinkBoard(Enum):
     TL3218X = auto()
     TL3218X_ML3M = auto()
     TL3218X_RETENTION = auto()
+    TL3238X = auto()
+    TL3238X_ML3M = auto()
+    TL3238X_RETENTION = auto()
     TL7218X = auto()
     TL7218X_ML7G = auto()
     TL7218X_ML7M = auto()
@@ -141,6 +144,12 @@ class TelinkBoard(Enum):
             return 'tl3218x_ml3m'
         elif self == TelinkBoard.TL3218X_RETENTION:
             return 'tl3218x_retention'
+        elif self == TelinkBoard.TL3238X:
+            return 'tl3238x'
+        elif self == TelinkBoard.TL3238X_ML3M:
+            return 'tl3238x_ml3m'
+        elif self == TelinkBoard.TL3238X_RETENTION:
+            return 'tl3238x_retention'
         elif self == TelinkBoard.TL7218X:
             return 'tl7218x'
         elif self == TelinkBoard.TL7218X_ML7G:
